c63007d4782e85c2e45b986b9b665b2e596ba3a3
ingastrelnikova <ingastrelnikova@yahoo.com>
2024-07-29 03:05:32 +0200

    fixed error

--- benchmark_reading_metrics/scripts/2_run_db.sh
+++ benchmark_reading_metrics/scripts/2_run_db.sh
@@ -4,7 +4,7 @@
 VM_USER="ingastrelnikova28"
 VM_NAME="db-load-vm"  # Update this to the correct VM name
 ZONE="europe-west10-a"
-REMOTE_SCRIPT_PATH="/home/$VM_USER/app/benchmark_writing_db/scripts/setup_db.sh"
+REMOTE_SCRIPT_PATH="/home/$VM_USER/app/benchmark_reading_metrics/scripts/setup_db.sh"
 
 # SSH into the VM and run the setup script
 gcloud compute ssh $VM_USER@$VM_NAME --zone=$ZONE --command="sudo chmod +x $REMOTE_SCRIPT_PATH && sudo bash $REMOTE_SCRIPT_PATH"
