71c70d76e3ac8fa53c4e9995622653d4675c8ae3
ingastrelnikova <ingastrelnikova@yahoo.com>
2024-07-31 21:06:25 +0200

    added access control

--- benchmark_access_control/scripts/2_run_experiment.sh
+++ benchmark_access_control/scripts/2_run_experiment.sh
@@ -6,5 +6,5 @@ VM_NAME="access-ctrl"
 ZONE="europe-west10-a"
 
 # SSH into the VM and run the setup script
-gcloud compute ssh $VM_USER@$VM_NAME --zone=$ZONE --command="sudo chmod +x chmod +x /home/$VM_USER/app/benchmark_access_control/scripts/setup_experiment.sh && sudo bash /home/$VM_USER/app/benchmark_access_control/scripts/setup_experiment.sh"
+gcloud compute ssh $VM_USER@$VM_NAME --zone=$ZONE --command="sudo chmod +x /home/$VM_USER/app/benchmark_access_control/scripts/setup_experiment.sh && sudo bash /home/$VM_USER/app/benchmark_access_control/scripts/setup_experiment.sh"
 
