309650c0665f79d1499269d3efd494e4a2f8dd80
ingastrelnikova <ingastrelnikova@yahoo.com>
2024-07-31 21:38:44 +0200

    added access control

--- benchmark_access_control/ResearchService/research_service.py
+++ benchmark_access_control/ResearchService/research_service.py
@@ -9,11 +9,11 @@ import csv
 app = Flask(__name__)
 
 # Function to write latency data to CSV
-def log_latency(start_time, end_time, operation):
+def log_latency(start_time, end_time, operation, result):
     latency = (end_time - start_time) * 1000  # Convert to milliseconds
     with open('latencies.csv', mode='a', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow([start_time, end_time, latency, operation])
+        writer.writerow([start_time, end_time, latency, operation, result])
 
 # Database connection
 def get_db_connection():
@@ -39,15 +39,17 @@ def check_authorization():
     response = requests.post(opa_url, json=input_data)
     response_json = response.json()
     end_time = time.time()
-    log_latency(start_time, end_time, "policy checking")
+    result = response_json.get("result", False)
+    log_latency(start_time, end_time, "policy checking",result)
     sys.stdout.flush()
-    return response_json.get("result", False)
+    return result
 
 # Endpoint to retrieve data
 @app.route('/data', methods=['GET'])
 def get_data():
     sys.stdout.flush()
-    if not check_authorization():
+    result = check_authorization()
+    if not result:
         sys.stdout.flush()
         return "Unauthorized", 403
     try:
@@ -59,7 +61,7 @@ def get_data():
         cursor.close()
         conn.close()
         end_time = time.time()
-        log_latency(start_time, end_time, "data retrieval")
+        log_latency(start_time, end_time, "data retrieval", result)
         return jsonify(rows)
     except Exception as e:
         return str(e), 500
@@ -68,5 +70,5 @@ if __name__ == '__main__':
     # Ensure CSV file has a header
     with open('latencies.csv', mode='w', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow(["start_time", "end_time", "latency_ms", "operation"])
+        writer.writerow(["start_time", "end_time", "latency_ms", "operation", "authorized"])
     app.run(debug=True, host='0.0.0.0', port=3002)
